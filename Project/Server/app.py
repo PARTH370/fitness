4608d87d34399adc502be8dd8ade7a823040025d
PARTHKUMAR <parthchodvadiya370@gmail.com>
2022-06-10 12:31:23 +0530

    make it better

--- Project/Server/app.py
+++ Project/Server/app.py
@@ -2,7 +2,7 @@ import os
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from Server.Views.Tags import router as Tag
+from .Views.Tags import router as Tag
 from Views.Post import router as Post
 from Views.User import router as User
 from Views.Goals import router as Goals
@@ -13,7 +13,7 @@ from Views.Exercise import router as Exercise
 from Views.Categories import router as Category
 from Views.Body_parts import router as Bodyparts
 from Views.Equipments import router as Equipments
-from Views.Subscription import router as Subscription
+from .Views.Subscription import router as Subscription
 from Utils.Payment import router as Razorpay
 
 from fastapi.middleware.cors import CORSMiddleware
