255655460868a5e6c3f18964ed253a84d79f81bf
PARTHKUMAR <parthchodvadiya370@gmail.com>
2022-06-10 13:00:56 +0530

    make it better

--- Project/Server/Utils/Image_Handler.py
+++ Project/Server/Utils/Image_Handler.py
@@ -4,7 +4,7 @@ import uuid
 
 async def Image_Converter(Hax_Value):
     random_name = str(uuid.uuid4())
-    decodeit = open(f"Server/static/{random_name}.jpg", 'wb')
+    decodeit = open(f"Project/Server/static/{random_name}.jpg", 'wb')
     decodeit.write(base64.b64decode(Hax_Value))
     decodeit.close()
     img_path = "http://localhost:8000/images?id=Server%2Fstatic%2F" + \
