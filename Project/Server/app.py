3e255693c966d952c43a90fda3924857e43db83c
PARTHKUMAR <parthchodvadiya370@gmail.com>
2022-06-10 12:21:38 +0530

    make it better

--- Project/Server/app.py
+++ Project/Server/app.py
@@ -3,18 +3,18 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 # from Server.Views.Tags import router as Tag
-from Views.Post import router as Post
-from Views.User import router as User
-from Views.Goals import router as Goals
-from Views.Recipe import router as Recipe
-from Views.Levels import router as Levels
-from Views.Workouts import router as Workout
-from Views.Exercise import router as Exercise
-from Views.Categories import router as Category
-from Views.Body_parts import router as Bodyparts
-from Views.Equipments import router as Equipments
-from Views.Subscription import router as Subscription
-from Utils.Payment import router as Razorpay
+# from Views.Post import router as Post
+# from Views.User import router as User
+# from Views.Goals import router as Goals
+# from Views.Recipe import router as Recipe
+# from Views.Levels import router as Levels
+# from Views.Workouts import router as Workout
+# from Views.Exercise import router as Exercise
+# from Views.Categories import router as Category
+# from Views.Body_parts import router as Bodyparts
+# from Views.Equipments import router as Equipments
+# from Views.Subscription import router as Subscription
+# from Utils.Payment import router as Razorpay
 
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -36,19 +36,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(Workout, tags=["Workout"], prefix="/Workout")
-app.include_router(User, tags=["User"], prefix="/User")
-app.include_router(Exercise, tags=["Exercise"], prefix="/Exercises")
-app.include_router(Bodyparts, tags=["Bodyparts"], prefix="/Bodyparts")
-app.include_router(Recipe, tags=["Recipe"], prefix="/Recipe")
-app.include_router(Equipments, tags=["Equipments"], prefix="/Equipments")
-app.include_router(Category, tags=["Categories"], prefix="/Categories")
-# app.include_router(Tag, tags=["Tags"], prefix="/Tags")
-app.include_router(Levels, tags=["Levels"], prefix="/Levels")
-app.include_router(Goals, tags=["Goals"], prefix="/Goals")
-app.include_router(Post, tags=["Post"], prefix="/Post")
-app.include_router(Subscription, tags=["Subscription"], prefix="/Subscription")
-app.include_router(Razorpay, tags=["Razorpay"], prefix="/Payments")
+# app.include_router(Workout, tags=["Workout"], prefix="/Workout")
+# app.include_router(User, tags=["User"], prefix="/User")
+# app.include_router(Exercise, tags=["Exercise"], prefix="/Exercises")
+# app.include_router(Bodyparts, tags=["Bodyparts"], prefix="/Bodyparts")
+# app.include_router(Recipe, tags=["Recipe"], prefix="/Recipe")
+# app.include_router(Equipments, tags=["Equipments"], prefix="/Equipments")
+# app.include_router(Category, tags=["Categories"], prefix="/Categories")
+# # app.include_router(Tag, tags=["Tags"], prefix="/Tags")
+# app.include_router(Levels, tags=["Levels"], prefix="/Levels")
+# app.include_router(Goals, tags=["Goals"], prefix="/Goals")
+# app.include_router(Post, tags=["Post"], prefix="/Post")
+# app.include_router(Subscription, tags=["Subscription"], prefix="/Subscription")
+# app.include_router(Razorpay, tags=["Razorpay"], prefix="/Payments")
 
 @app.get("/images", tags=["IMAGE"])
 def get_images(id):
